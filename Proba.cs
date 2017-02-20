32936076c4bdb3bd7e350a85ec62a96351f96e01
Erik Gúth <guthember@gmail.com>
2017-02-20 10:22:10 +0100

    Megoldva

---
+++ Proba.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ism
+{
+    // + public, - private (alapértelmezett)
+    class Proba
+    {
+        // Adattagok
+        private int x;
+        private int y;
+
+        public int X
+        {
+            get 
+            { 
+                return x; 
+            }
+            set 
+            {
+                if (value < 1)
+                {
+                    x = 1;
+                }
+                else
+                {
+                    x = value;
+                }
+            }
+        }
+
+        public int Y
+        {
+            get 
+            { 
+                return y; 
+            }
+            set
+            { 
+            }
+
+        }
+ 
+        // Metódusok
+        // Konstruktor
+        //public Proba(int a, int b)
+        //{
+            
+
+        //    if (b < 1)
+        //    {
+        //        y = 1;
+        //    }
+        //    else
+        //    {
+        //        y = b;
+        //    }
+
+        //}
+       
+        public int Osszead(  )
+        {
+            return x + y;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ism
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a, b;
+            Proba p;
+            Console.Write("Kerek egy egesz szamot:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Kerek egy egesz szamot:");
+            b = Convert.ToInt32(Console.ReadLine());
+            
+            p = new Proba();
+
+            p.X = a;
+            p.Y = b;
+            Console.WriteLine("{0}+{1}={2}",p.X, p.Y, p.Osszead());
+ 
+            Console.ReadKey();
+            
+        }
+    }
+}
